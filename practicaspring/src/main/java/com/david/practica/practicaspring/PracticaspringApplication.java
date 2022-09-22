94d9f663939a90f0fd0a011c023e44cce7dc1a62
Dncanulm <dncanulm@liverpool.com.mx>
2022-09-22 10:34:01 -0500

    se agrega projecto spring

---
+++ practicaspring/src/main/java/com/david/practica/practicaspring/PracticaspringApplication.java
@@ -0,0 +1,13 @@
+package com.david.practica.practicaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaspringApplication.class, args);
+	}
+
+}
